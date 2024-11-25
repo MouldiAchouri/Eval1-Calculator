abf95c6284e5f3e42e41597bd1aafb976761ce0d
Mouldi Achouri <pv37dbn@eduvaud.ch>
2024-11-25 11:56:51 +0100

    feat: power operator

--- src/main.py
+++ src/main.py
@@ -6,19 +6,27 @@ def main():
     ask_user_input()
     result = calculate(operand1, operator, operand2)
     display_result(operand1, operator, operand2, result)
+    ask_user_float_input(float)
 
+def ask_user_float_input(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 def ask_user_input():
     # Get first operand from the user
     global operand1
-    operand1 = float(input("Enter the first operand: "))
+    operand1 = ask_user_float_input("Enter the first operand: ")
 
     global operator
     # Get the operator from the user
-    operator = input("Enter an operator (+, -, *, /): ")
+    operator = input("Enter an operator (+, -, *, /, **): ")
 
     global operand2
     # Get second operand from the user
-    operand2 = float(input("Enter the second operand: "))
+    operand2 = ask_user_float_input("Enter the second operand: ")
+
 
 def calculate(ope1, oper, ope2):
     # Perform the operation based on the operator
@@ -34,6 +42,8 @@ def calculate(ope1, oper, ope2):
                 print("Error: Division by zero is undefined.")
                 return
             res = ope1 / ope2
+        case '**':
+            res = ope1 ** ope2
         case _:
             print("Invalid operator.")
             return
@@ -41,6 +51,7 @@ def calculate(ope1, oper, ope2):
 
 def display_result(op1, ope, ope2, res):
     print(str(op1) + " " + ope + " " + str(ope2) + " = " + str(res))
+
 
 # Call the main function to run the program
 main()
